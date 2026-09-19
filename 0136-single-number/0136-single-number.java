be3680a42a5842aa0b542bc885b1f60743f1d4e7
Shantnu Pakira <shantnupdav12@gmail.com>
2026-09-19 20:54:17 +0530

    Time: 1 ms (99.94%), Space: 47.1 MB (14.01%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+        for(int i = 0 ; i<=nums.length-1 ; i++){
+            result = result ^ nums[i] ;
+
+        }
+        return result;
+
+        
+    }
+}
